09dae4ae538d60ff6dcbba8d34e16a54476ba21e
rajeshjadidminc <95206513+rajeshjadidminc@users.noreply.github.com>
2022-06-20 22:43:36 +0530

    Updates

---
+++ precache-manifest.bf62d336e85a313b0a82fdb56ab9feb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d988073dce7c687df838",
+    "url": "/ReactSample/static/css/main.ab77395a.chunk.css"
+  },
+  {
+    "revision": "d988073dce7c687df838",
+    "url": "/ReactSample/static/js/main.d988073d.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/ReactSample/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "1217ad87c0edf883da90",
+    "url": "/ReactSample/static/css/2.26f8a970.chunk.css"
+  },
+  {
+    "revision": "1217ad87c0edf883da90",
+    "url": "/ReactSample/static/js/2.1217ad87.chunk.js"
+  },
+  {
+    "revision": "143c295e0d4508258d50",
+    "url": "/ReactSample/static/js/runtime~main.143c295e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ReactSample/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "19143544651ef4c45835efb6dc26e65c",
+    "url": "/ReactSample/index.html"
+  }
+];
